2962b74207530dd93cb8dc503372ef90f51e2fbd
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-11-11 18:35:27 +0100

    personneS :)

---
+++ src/Controller/PersonnesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Personne;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnesController extends AbstractController
+{
+    /**
+     * @Route("/personnes", name="personnes")
+     */
+    public function index(): Response
+    {
+
+        //recuperer le repository
+        $repository=$this->getDoctrine()->getRepository(Personne::class);
+        //je lis la BDD
+        $personnes=$repository->findALL();
+
+        return $this->render('personnes/_personnes.html.twig', [
+            'personnes' => $personnes,
+        ]);
+    }
+}
