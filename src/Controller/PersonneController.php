98d37bd73b98992d6103861d7f92778d975cbd2b
Hugo.D <hugo.divet@epsi.fr>
2020-11-11 18:27:39 +0100

    Page d'accueil finie mais variable "personne" soit disant 'non définie'.

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PersonneController extends AbstractController
+class PersonnesController extends AbstractController
 {
     /**
-     * @Route("/personne", name="personne")
+     * @Route("/", name="personne")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('personne/_personne.html.twig', [
-            'controller_name' => 'PersonneController',
+        $repo = $this->getDoctrine()->getRepository(Personne::class);
+
+        //récupérer toutes les catégories
+        $personnes = $repo->findAll();
+
+        echo $this->render('home/index.html.twig', [
+            'personne' => $personnes,
         ]);
     }
 }
