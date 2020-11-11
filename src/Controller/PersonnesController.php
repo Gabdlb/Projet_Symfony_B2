7823ebc50ab26ce7ee47d03de0f380cf7bb8fa48
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-11-11 19:24:09 +0100

    ajout/modif/suppr

--- src/Controller/PersonnesController.php
+++ src/Controller/PersonnesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Personne;
+use App\Form\PersonneSupprimerType;
+use App\Form\PersonneType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +26,104 @@ class PersonnesController extends AbstractController
 
         return $this->render('personnes/_personnes.html.twig', [
             'personnes' => $personnes,
+        ]);
+    }
+
+    /**
+     * @Route("/personnes/ajouter",name="personnes_ajouter")
+     */
+    public function ajouter(Request $request){
+
+        $personnes=new Personne();
+
+        //creation du formulaire
+        $form=$this->createForm(PersonneType::class, $personnes);
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //récupération de l'entity
+            $em=$this->getDoctrine()->getManager();
+
+
+            $em->persist($personnes);
+
+            //dire au manager qu'on veut garder notre objet en BDD
+            $em->flush();
+
+            //aller à la liste des chatons
+            return $this->redirectToRoute("personnes");
+        }
+
+        return $this->render("personnes/personnes_ajouter.html.twig", [
+            "formulaire"=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("personnes/modifier/{id}",name="chatons_modifier")
+     */
+    public function modifier($id, Request $request)
+    {
+
+        $repo=$this->getDoctrine()->getRepository(Personne::class);
+        $personnes=$repo->find($id);
+
+
+        $form=$this->createForm(PersonneType::class, $personnes);
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //récupération de l'entity
+            $em=$this->getDoctrine()->getManager();
+
+            //dire au manager qu'on veut garder notre objet en BDD
+            $em->persist($personnes);
+
+            //generer l'update
+            $em->flush();
+
+            //
+            return $this->redirectToRoute("personnes",["idPersonnes"=>$personnes->gePersonne()->getId()]);
+        }
+
+        return $this->render("personnes/personnes_modifier.html.twig", [
+            "formulaire"=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/personnes/supprimer/{id}",name="personnes_supprimer")
+     */
+    public function supprimer($id, Request $request)
+    {
+        //Aller chercher la catégorie dans la BDD
+        $repo=$this->getDoctrine()->getRepository(Personne::class);
+        $personne = $repo->find($id);
+
+        //création du formulaire
+        $form = $this->createForm(PersonneSupprimerType::class, $personne);
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //récupération de l'entityManager
+            $em = $this->getDoctrine()->getManager();
+
+            //dire au manager qu'on veut garder notre objet en BDD
+            $em->remove($personne);
+
+
+            $em->flush();
+
+
+            return $this->redirectToRoute("categories");
+        }
+
+        return $this->render("personnes/personnes_supprimer.html.twig", [
+            "formulaire" => $form->createView(),
+            "personne"=>$personne
         ]);
     }
 }
---
+++ src/Form/PersonneSupprimerType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personne;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonneSupprimerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("ok", SubmitType::class, ["label"=>"Supprimer"])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Personne::class,
+        ]);
+    }
+}
