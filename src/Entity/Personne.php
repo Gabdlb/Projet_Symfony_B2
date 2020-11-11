5e317230a366b3dcaee5247e408c1c67161d8250
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-11-11 16:02:28 +0100

    entity

---
+++ src/Entity/Personne.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonneRepository::class)
+ */
+class Personne
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $dette;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $arecevoir;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDette(): ?float
+    {
+        return $this->dette;
+    }
+
+    public function setDette(float $dette): self
+    {
+        $this->dette = $dette;
+
+        return $this;
+    }
+
+    public function getArecevoir(): ?float
+    {
+        return $this->arecevoir;
+    }
+
+    public function setArecevoir(float $arecevoir): self
+    {
+        $this->arecevoir = $arecevoir;
+
+        return $this;
+    }
+}
