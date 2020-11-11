f3f2576aa0293cdad164f24fd3198c89f311f196
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-11-11 14:51:39 +0100

    bdd

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     /**
-     * @Route("/menu", name="menu")
+     * @Route("", name="menu")
      */
     public function index(): Response
     {
-        return $this->render('menu/index.html.twig', [
+        return $this->render('menu/_menu.html.twig', [
             'controller_name' => 'MenuController',
         ]);
     }
